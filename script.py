8583fc3a6193990f3d4815c6a2f8de37864a329f
amirsoleix <asoleix@gmail.com>
2024-04-14 23:46:35 +0330

    Add members

--- script.py
+++ script.py
@@ -594,3 +594,9 @@ st.markdown("""
 - [تیتر خبری دوآب](https://roozno.com/fa/news/32826/%D8%A7%D8%B2-%D8%AF%D9%88%D8%A2%D8%A8-%DA%A9%D8%AC%D9%88%D8%B1-%D8%AA%D8%A7-%DA%AF%D9%84%D9%86%D8%AF%D8%B1%D9%88%D8%AF-%D8%B1%D8%B4%D8%AA%D9%87%E2%80%8C%D8%A7%DB%8C-%D8%A8%D8%A7-%D8%B1%D9%88%D8%B3%D8%AA%D8%A7%D9%87%D8%A7%DB%8C-%D8%B2%DB%8C%D8%A8%D8%A7)
 - [جاذبه‌های گردشگری مازندران](http://evaz2mn.blogfa.com/category/15/-%D9%84%DB%8C%D8%B3%D8%AA-%DA%A9%D8%A7%D9%85%D9%84-%D8%A7%D8%B3%D8%A7%D9%85%DB%8C-%D8%B1%D9%88%D8%B3%D8%AA%D8%A7%D9%87%D8%A7%DB%8C-%D9%85%D9%86%D8%B7%D9%82%D9%87-%DA%A9%D8%AC%D9%88%D8%B1-)
 """)
+st.header("اعضای گروه")
+st.markdown("""
+- امیرمهدی سلیمانی‌فر (98101747)
+- لیلی مطهری (99171214)
+- رابعه پرهیزکاری (400109413)
+""")
